94fbda28b8c8b73aa61d63132b88f872331f2830
Dylan Owen <dylan.owen@workday.com>
2019-09-07 13:54:38 -0700

    Initial Commit

---
+++ automaton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  automaton
+//
+//  Created by Dylan Owen on 9/7/19.
+//  Copyright © 2019 Dylan Owen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
